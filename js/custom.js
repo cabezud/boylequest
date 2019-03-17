7beae4f5d1aeb55532be5240b063e0b171cb7fd9
cabezud <cabezud@hotmail.com>
2019-03-17 09:44:04 -0700

    game lookin good

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,33 @@
 var loc = 0;
 var score = 0;
+var pts = 10;
 
-$('.btn-im-here,.btn-judge-answer,.answer-box,.pts-value').hide();
+function changeStartLoc(n) {
+	var shifted = quest.splice(n);
+	quest = shifted.concat(quest);
+	quest.push( finalQuestion );
+};
+$('.pts-section').hide();
+$('.group-a').click(function(){
+	changeStartLoc(0);
+	nextLocation(); $('.pts-section').show();
+});
+$('.group-b').click(function(){
+	changeStartLoc(7);
+	nextLocation(); $('.pts-section').show();
+});
+$('.group-c').click(function(){
+	changeStartLoc(14);
+	nextLocation(); $('.pts-section').show();
+});
+$('.group-d').click(function(){
+	changeStartLoc(18);
+	nextLocation(); $('.pts-section').show();
+});
+
+$('.btn-next-location, .btn-im-here, .btn-judge-answer, .answer-box').hide();
 $('.btn-next-location').click(function() {
-	console.log('.btn-next-location fired');
-	nextLocation();
+	nextLocation();	
 });
 $('.btn-im-here').click(function() {
 	imHere();
@@ -14,61 +37,62 @@ $('.btn-judge-answer').click(function() {
 });
 
 function nextLocation() {
-	loc++;
 	$('.btn-next-location').hide();
-	$('.txt').text("Find Location");
-	$('.q-num').text(loc);
-	$('.quest-box').html(quest[loc-1]["f"]);
-	$('.btn-im-here').show();
+
+	loc++;
+	pts = 10;
+	if (loc == quest.length) { pts = 50; }; //final question worth 50
+
+	if (loc > quest.length) { 
+		$('.txt').text('Complete.');
+		$('.q-num').text('');
+	} else {
+		updatePtsSection();
+		$('.txt').text("Find Location");
+		$('.q-num').text(loc);
+		$('.quest-box').html( quest[loc-1]["f"] );
+		$('.btn-im-here').show();
+	}
 };
-function imHere() {
+function imHere() { console.log("imHere fired.");
 	$('.btn-im-here').hide();
+
+	updatePtsSection();
+	if (pts == 5) {
+		$('.hint').html('<button onclick="showHint()"">Show Hint (costs 2 pts)</button>');
+	}
+
 	$('.txt').text("Question");
 	$('.quest-box').html(quest[loc-1]["q"]);
-	$('.pts-value span').text(quest[loc-1]["pts"]);
-	$('.pts-value').show();
-	$('.btn-judge-answer,.answer-box').show();
+
+	$('.btn-judge-answer, .answer-box').show();
 };
 function judgeAnswer() {
-	$('.btn-judge-answer,.answer-box,.pts-value').hide();
-	console.log( $('.answer-box').val() );
-	console.log( quest[loc-1]["a"] );
-	if ($('.answer-box').val() == quest[loc-1]["a"]) {
-		score += quest[loc-1]["pts"];
-		$('p.quest-box').html(quest[loc-1]["right"]);
-	} else {
-		score += 5;
-		$('p.quest-box').html(quest[loc-1]["wrong"]);
-	};
+	$('.btn-judge-answer,.answer-box').hide();
 
-	$('.pts').text(score);
-	$('.answer-box').val('');
-	$('.btn-next-location').show();
+	//if (true) {
+	if ($('.answer-box').val().toLowerCase().trim() == quest[loc-1]["a"]) { console.log('right');
+		score += pts;
+		pts = 0;
+		updatePtsSection();
+
+		$('.quest-box').html(quest[loc-1]["r"]);		
+		$('.answer-box').val('');
+		$('.hint').html('');
+		$('.btn-next-location').show();
+	} else { console.log('wrong');
+		pts = Math.min(5,pts);
+		$('.answer-box').val('');
+		$('.quest-box').html('Sorry. <button onclick="imHere()">Try Again</button>');
+	};	
 };
-/*
-var initCustom = function() {
-	var url_string = window.location.href;
-	var url = new URL(url_string);
-
-	//get data from parameters
-	var q = url.searchParams.get("q");
-	var s = url.searchParams.get("s");
-
-	if (q == null) {
-		q = 0;
-	};
-	if (s == null) {
-		s = 0;
-	};
+function showHint() {
+	pts = Math.max(1,pts - 2);
+	updatePtsSection();
+	$('.hint').text( quest[loc-1]["h"] );
 };
-
-window.addEventListener( 'DOMContentLoaded', initCustom, false);
-*/
-/*
-	var url_string = window.location.href;
-	var url = new URL(url_string);
-	var q = url.searchParams.get("q");
-	q = q++;
-	url.searchParams.set("q") = q;
-	window.location.href(url);
-*/
+function updatePtsSection() {
+	$('.pts').text( score );
+	$('.q-num').text( loc );
+	$('.pts-value').text( pts );
+};
--- js/quest.js
+++ js/quest.js
@@ -1,94 +1,136 @@
 var quest = [
 	{
-		"f":"Take me out to the ball game, take me out to the park.<br>Buy me some peanuts and Cracker Jacks, I don’t care if I ever come back.<br>For it’s root, root, root for the ____ team",
+		"f":"Take me out to the ball game, take me out to the crowd.<br>Buy me some peanuts and Cracker Jacks, I don't care if I ever come back.<br>For it's root, root, root for the ____ team.",
 		"q":"Mill Valley Little League provides a sign with some advice for people watching a game. How many different *roles* are mentioned in their advice?",
 		"a":"6",
-		"pts":10,
-		"right":"Right! 6 (players, professionals, managers, coaches, umpires, volunteers)",
-		"wrong":"Wrong. 6 (players, professionals, managers, coaches, umpires, volunteers). We'll give you a few points for getting close."
+		"h":"don't forget the 'players' and 'professionals'",
+		"r":"Right!\n6 (players, professionals, managers, coaches, umpires, volunteers)"
 	},
 	{
 		"f":"If both baseball fields are taken, players sometimes use a practice chain-link backstop to warm up. Find this practice area, and the bench near it dedicated to Marga Wells McLeod.",
-		"q":"When very large redwood trees are cut down, a group of smaller trees may grow in its place, sometimes called a ‘fairy ring’. There is a very large such ring behind the bench where you are standing. How many (live) trees have grown in this grove?",
+		"q":"When very large redwood trees are cut down, a group of smaller trees may grow in its place, sometimes called a 'fairy ring'. There is a very large such ring behind the bench where you are standing. How many (live) trees have grown in this grove?",
 		"a":"26",
-		"pts":30,
-		"right":"Right! I can't believe you counted all those trees!?",
-		"wrong":"Wrong. That was a super hard one, but we counted 26 trees!"
+		"h":"Count anything any redwood truck that is growing out of the ground, even if it's skinny. The answer is an even number, and more than 20. ",
+		"r":"Right!\nI can't believe you counted all those trees!?"
 	},
 	{
-		"f":"Baseball games are fun for fans, but there are too many cars! Boyle Park created extra parking spots with a gravel lot. Find this gravel lot.",
-		"q":"Not all of this parking lot is gravel. A pile of this grainy substance also occupies the lot, along with a nearby tool. Name this substance and tool (2 answers)",
-		"a":"shovel",
-		"pts":10,
-		"right":"Right!",
-		"wrong":"Wrong. Answer: Sand and Shovel"
-	},
-	{
-		"f":"Enough about sand for adults. Find the sand meant for kids! ",
-		"q":" Two pyramids stand in the sky with moss growing on only one side. Which side, north, south, east, or west?",
+		"f":"Enough about sand for adults. Find the sand meant for kids!",
+		"q":"Two pyramids stand in the sky with moss growing on only one side. Which side?",
 		"a":"north",
-		"pts":10,
-		"right":"Right! Fun fact, moss often grows on the north side of trees because there is more shade (in the northern hemisphere)",
-		"wrong":"Wrong. Answer: north"
+		"h":"north, south, east, or west?",
+		"r":"Right!\nFun fact, moss often grows on the north side of trees because there is more shade (in the northern hemisphere)"
 	},
 	{
-		"f":"Congrats on making it this far, you deserse a hand! Look around, and you’ll see lots of hands, imprinted by children of families that have supported Boyle Park. Can you find three hands together?",
+		"f":"Congrats on making it this far, you deserve a hand! Look around, and you'll see lots of hands, imprinted by children of families that have supported Boyle Park. Can you find three hands together?",
 		"q":"Starting on the square with three hands, move nine squares to the right. What is the sum of the numbers on that square?",
-		"a":2048,
-		"pts":10,
-		"right":"Right! ",
-		"wrong":"Wrong. "
+		"a":"2048,",
+		"h":"ask an adult to use their calculator",
+		"r":"Right!"
 	},
 	{
-		"f":"*Rock* solid job everyone. Debbie Joseph and her family would be proud. Maybe someday you’ll have a plaque too.",
-		"q":"The dedication on this plaque is 8 lines long (not counting the dates at the bottom).\nIf you were to number each line of this dedication, 1 through 8, and each letter of each line, starting with 1 on each line, what secret word is revealed by the following cypher?\n7-3  1-16  5-10  3-3  7-4  5-15  3-20  5-18",
+		"f":"*Rock* solid job everyone. Debbie Joseph and her family would be proud. Maybe someday you'll have a plaque too.",
+		"q":"The dedication on this plaque is 8 lines long.\nIf you were to number each line of this dedication, 1 through 8, and each letter of each line, starting with 1 on each line, what secret word is revealed by the following cypher?\n7-3  1-16  5-10  3-3  7-4  5-15  3-20  5-18",
 		"a":"baseball",
-		"pts":10,
-		"right":"Right!",
-		"wrong":"Wrong. The answer was 'baseball'."
+		"h":"The 7th line starts with the word 'Debbie', the 3rd letter of which is 'b'. So the answer starts with 'b'.",
+		"r":"Right!"
 	},
 	{
-		"f":"You’ve all made it so far, I’m getting MAD. MAD, MAD, MAD! I feel like writing MAD on a sign!",
-		"q":"MAD is spelled in all capital letters. What other word do you see spelled in all capital letters on this sign?",
-		"a":"BBQ",
-		"pts":10,
-		"right":"Right! ",
-		"wrong":"Wrong. "
+		"f":"You've all made it so far, I'm getting MAD. MAD, MAD, MAD! I feel like creating the word MAD somewhere!",
+		"q":"There are other words you can make using this tool.  Can you make a word that goes with BBQ?",
+		"a":"pit",
+		"h":"BBQ's often need a hollow indentation or hole in the ground, otherwise known as a ___. ",
+		"r":"Right!"
 	},
 	{
 		"f":"BBQ time. Were in the park can you find at least three BBQs close together?",
-		"q":"Assuming there’s not a party going on, grab a seat, you’ve got a puzzle to solve!\nHow many different words can be made by removing letters from the phrase \n\“Mill Valley\”\nwithout rearranging any of the letters? (you can ignore the space in between)\n<i>For example, “Sand” could be made into ‘sad’, ‘and’, ‘an’, but not ‘as’</i>.",
-		"a":"13 (mill, mile, mall, male, may, me, my, ill, lay, vale, valley, all, alley)",
-		"pts":10,
-		"right":"Right! ",
-		"wrong":"Wrong. "
+		"q":"Assuming there's not a party going on, grab a seat, you've got a puzzle to solve!\nHow many different words can be made by removing letters from the phrase \n\"Mill Valley\"\nwithout rearranging any of the letters? (you can ignore the space in between)\n<i>For example, \"Sand\" could be made into 'sad', 'and', 'an', but not 'as'</i>.",
+		"a":"13",
+		"h":"There are more than 10 words. Did you see 'lay', 'vale' and 'alley'?",
+		"r":"Right!\n(mill, mile, mall, male, may, me, my, ill, lay, vale, valley, all, alley)"
 	},
 	{
 		"f":"Boyle Park is cut in half by a creek. Today, you can cross this creek by a wooden footbridge. A much older footbridge, though, has since been torn down. You can still find its cement supports on the side of the creek. Find these cement supports.",
-		"q":"What animal name is written in the cemet?",
-		"a":"Lions",
-		"pts":10,
-		"right":"Right! ",
-		"wrong":"Wrong. "
+		"q":"The name of what animals is written in the cement?",
+		"a":"lions",
+		"h":"You may have to look on both sides of the creek.",
+		"r":"Right!"
 	},
 	{
-		"f":"Cross the wooden footbridge (not the one that was torn down!) to reach the west bank of the creek. When you’re there, find the sign with drawings of *both* fish and birds.",
+		"f":"Cross the wooden footbridge (not the one that was torn down!) to reach the west bank of the creek, if you're not already there. Find the sign with drawings of *both* fish and birds.",
 		"q":"How many circles do you see?",
 		"a":"15",
-		"pts":10,
-		"right":"Right! ",
-		"wrong":"Wrong. "
-	}, //Q (5): According to this sign, where does the water in this creek end up? Richardson Bay
+		"h":"There are more than a dozen, but less than 20.",
+		"r":"Right!"
+	},
 	{
 		"f":"Speaking of fish, find another sign near the creek with a drawing of fish.",
 		"q":"What is the sum of the numbers found on this sign (adding each number individually)?",
 		"a":"47",
-		"pts":10,
-		"right":"Right! ",
-		"wrong":"Wrong. "
-	}	
+		"h":"It is also the sum of 10+9+8+7+6+5+4-3+2-1.",
+		"r":"Right! Ok, math class is over."
+	},
+	{
+		"f":"A sign made of wood provides the phone number to reserve the Boyle Park picnic area. Find this sign.",
+		"q":"Writing the letter F requires three strokes, the top line, middle line and vertical line. How many strokes did the person carving this sign have to make? ",
+		"a":"73",
+		"h":"It more than 70 strokes! The number 8 took three strokes.",
+		"r":"Right!"
+	},
+	{
+		"f":"Now, don't move! Well, you can turn around if you want. From where you are standing, can you see a yellow diamond shape? Go there!",
+		"q":"Important people need to be acknowledged. Who is the large area next to you named for?",
+		"a":"pete brindley",
+		"h":"Did you look up around the chain link fence? Be sure to include the first and last name, with a space in between. ",
+		"r":"Right!"
+	},
+	{
+		"f":"Baseball players have a lot a equipement when they play. Where do they store their stuff when they are playing?",
+		"q":"How many hooks can you count?",
+		"a":"44",
+		"h":"Don't forget the other dugout!",
+		"r":"Right!"
+	},
+	{
+		"f":"How about a hot dog?",
+		"q":"The snack shop at the Buena Vista field if often run by local teenagers, who serve snacks to players, coaches, fans and the occasional pet dog. According to the City of Mill Valley, though, what animals are they *not* supposed to feed?",
+		"a":"coyotes",
+		"h":"There is a sign on the building with this instruction. ",
+		"r":"Right!"
+	},
+	{
+		"f":"Find the nearby plaque dedicated to Jenny Fulle.",
+		"q":"Read the plaque. When Jenny was 9 years old, she tucked her hair into her hat and tried to trick little league managers into letting her play. After a long battle, she was finally allowed to play when she was 11 years old. How old was she when this plaque was dedicated to her? ",
+		"a":"54",
+		"h":"The plaque was dedicated in 2017, and Jenny was born in 1963.",
+		"r":"Right!"
+	},
+	{
+		"f":"Oh say can you see?",
+		"q":"This flag is dedicated to Arthur Fontes, who graduated from Tam High School in 1939. He went on to fight in World War II for the Navy in the Pacific, before he was transferred to a military base in Chicago. But in his younger days, was a great athlete and baseball player. In his final few games playing for the North Bay League, Arthur had 13 hits in a row, including 5 home runs. What is the *minimum* number of bases that Arthur touched?",
+		"A":"29",
+		"h":"He hit 5 home runs touching 4 bases each, and another 8 hits worth at least 1 base each. And was there another base mentioned in the question?",
+		"r":"Right!"
+	},
+	{
+		"f":"After all this running around - I have to go to the bathroom! While I am there, I want to read about the restoration of Boyle Park in 1994!",
+		"q":"Say the word 'silk' three times. Then answer the question, what do cows drink?",
+		"a":"water",
+		"h":"Not milk.",
+		"r":"Right!"
+	},
+	{
+		"f":"Find a tree so large and hollow that you can stand inside of it!",
+		"q":"Note the hollow burned out trunk of the large bay tree. In the Fall of 2000, vandals set this tree on fire, but tree experts said the tree has been through several fires and is in no immediate danger. Take some free points and hear more about this great tree. Type 'fire' in the answer box. ",
+		"a":"fire",
+		"h":"The answer is 'fire'",
+		"r":"Right!\nMany believe this tree was one of three prominent trees that stood here since the 1800s. One of the three, an oak, died and was removed several years ago. The other, a very large and ancient looking oak, can be seen across the creek from this tree."
+	}
 ];
 
-
-
-
+var finalQuestion = {
+	"f":"FINAL QUESTION!!! No pressure, but this one is worth 50 points!\nReturn to the location of the three BBQs together.",
+	"q":"In what year was Robert born?",
+	"a":"2009",
+	"h":"What has been at the bottom of this screen all along?",
+	"r":"WINNERS!! You did it! I can't believe it! You have all you need to claim your prize."
+};

abbab9ddc437cb32529bb93b85d9b69b0212fb84
cabezud <cabezud@hotmail.com>
2019-03-17 12:17:03 -0700

    improving questions

--- js/quest.js
+++ js/quest.js
@@ -14,69 +14,6 @@ var quest = [
 		"r":"Right!\nI can't believe you counted all those trees!?"
 	},
 	{
-		"f":"Enough about sand for adults. Find the sand meant for kids!",
-		"q":"Two pyramids stand in the sky with moss growing on only one side. Which side?",
-		"a":"north",
-		"h":"north, south, east, or west?",
-		"r":"Right!\nFun fact, moss often grows on the north side of trees because there is more shade (in the northern hemisphere)"
-	},
-	{
-		"f":"Congrats on making it this far, you deserve a hand! Look around, and you'll see lots of hands, imprinted by children of families that have supported Boyle Park. Can you find three hands together?",
-		"q":"Starting on the square with three hands, move nine squares to the right. What is the sum of the numbers on that square?",
-		"a":"2048,",
-		"h":"ask an adult to use their calculator",
-		"r":"Right!"
-	},
-	{
-		"f":"*Rock* solid job everyone. Debbie Joseph and her family would be proud. Maybe someday you'll have a plaque too.",
-		"q":"The dedication on this plaque is 8 lines long.\nIf you were to number each line of this dedication, 1 through 8, and each letter of each line, starting with 1 on each line, what secret word is revealed by the following cypher?\n7-3  1-16  5-10  3-3  7-4  5-15  3-20  5-18",
-		"a":"baseball",
-		"h":"The 7th line starts with the word 'Debbie', the 3rd letter of which is 'b'. So the answer starts with 'b'.",
-		"r":"Right!"
-	},
-	{
-		"f":"You've all made it so far, I'm getting MAD. MAD, MAD, MAD! I feel like creating the word MAD somewhere!",
-		"q":"There are other words you can make using this tool.  Can you make a word that goes with BBQ?",
-		"a":"pit",
-		"h":"BBQ's often need a hollow indentation or hole in the ground, otherwise known as a ___. ",
-		"r":"Right!"
-	},
-	{
-		"f":"BBQ time. Were in the park can you find at least three BBQs close together?",
-		"q":"Assuming there's not a party going on, grab a seat, you've got a puzzle to solve!\nHow many different words can be made by removing letters from the phrase \n\"Mill Valley\"\nwithout rearranging any of the letters? (you can ignore the space in between)\n<i>For example, \"Sand\" could be made into 'sad', 'and', 'an', but not 'as'</i>.",
-		"a":"13",
-		"h":"There are more than 10 words. Did you see 'lay', 'vale' and 'alley'?",
-		"r":"Right!\n(mill, mile, mall, male, may, me, my, ill, lay, vale, valley, all, alley)"
-	},
-	{
-		"f":"Boyle Park is cut in half by a creek. Today, you can cross this creek by a wooden footbridge. A much older footbridge, though, has since been torn down. You can still find its cement supports on the side of the creek. Find these cement supports.",
-		"q":"The name of what animals is written in the cement?",
-		"a":"lions",
-		"h":"You may have to look on both sides of the creek.",
-		"r":"Right!"
-	},
-	{
-		"f":"Cross the wooden footbridge (not the one that was torn down!) to reach the west bank of the creek, if you're not already there. Find the sign with drawings of *both* fish and birds.",
-		"q":"How many circles do you see?",
-		"a":"15",
-		"h":"There are more than a dozen, but less than 20.",
-		"r":"Right!"
-	},
-	{
-		"f":"Speaking of fish, find another sign near the creek with a drawing of fish.",
-		"q":"What is the sum of the numbers found on this sign (adding each number individually)?",
-		"a":"47",
-		"h":"It is also the sum of 10+9+8+7+6+5+4-3+2-1.",
-		"r":"Right! Ok, math class is over."
-	},
-	{
-		"f":"A sign made of wood provides the phone number to reserve the Boyle Park picnic area. Find this sign.",
-		"q":"Writing the letter F requires three strokes, the top line, middle line and vertical line. How many strokes did the person carving this sign have to make? ",
-		"a":"73",
-		"h":"It more than 70 strokes! The number 8 took three strokes.",
-		"r":"Right!"
-	},
-	{
 		"f":"Now, don't move! Well, you can turn around if you want. From where you are standing, can you see a yellow diamond shape? Go there!",
 		"q":"Important people need to be acknowledged. Who is the large area next to you named for?",
 		"a":"pete brindley",
@@ -105,17 +42,17 @@ var quest = [
 		"r":"Right!"
 	},
 	{
-		"f":"Oh say can you see?",
-		"q":"This flag is dedicated to Arthur Fontes, who graduated from Tam High School in 1939. He went on to fight in World War II for the Navy in the Pacific, before he was transferred to a military base in Chicago. But in his younger days, was a great athlete and baseball player. In his final few games playing for the North Bay League, Arthur had 13 hits in a row, including 5 home runs. What is the *minimum* number of bases that Arthur touched?",
-		"A":"29",
-		"h":"He hit 5 home runs touching 4 bases each, and another 8 hits worth at least 1 base each. And was there another base mentioned in the question?",
+		"f":"After all this running around - I have to go to the bathroom - I choose the one near the baseball diamonds. While I am there, I want to read about the restoration of Boyle Park in 1994!",
+		"q":"On the plaque, imagine that each line of text has a number assigned to it - 1-31.  And if you gave a number to each letter starting from the left, follow this cipher - what does it spell?\n4-24  11-7  2-15  9-56  13-5  5-3  1-3",
+		"a":"varsity",
+		"h":"The 4th line starts with the word 'Terry', the 24th letter of which is ‘v’. So the answer starts with ‘v’",
 		"r":"Right!"
 	},
 	{
-		"f":"After all this running around - I have to go to the bathroom! While I am there, I want to read about the restoration of Boyle Park in 1994!",
-		"q":"Say the word 'silk' three times. Then answer the question, what do cows drink?",
-		"a":"water",
-		"h":"Not milk.",
+		"f":"Remember the last answer that you just gave. Can you see this word from where you are standing?",
+		"q":"What is the sum of the numbers next to this word?",
+		"a":"57",
+		"h":"The last answer was 'Varsity'. And remember to add each number individually.",
 		"r":"Right!"
 	},
 	{
@@ -124,7 +61,84 @@ var quest = [
 		"a":"fire",
 		"h":"The answer is 'fire'",
 		"r":"Right!\nMany believe this tree was one of three prominent trees that stood here since the 1800s. One of the three, an oak, died and was removed several years ago. The other, a very large and ancient looking oak, can be seen across the creek from this tree."
-	}
+	},
+	{
+		"f":"Find the sand meant for kids.",
+		"q":"Two pyramids stand in the sky with moss growing on only one side. Which side?",
+		"a":"north",
+		"h":"north, south, east, or west?",
+		"r":"Right!\nFun fact, moss often grows on the north side of trees because there is more shade (in the northern hemisphere)"
+	},
+	{
+		"f":"Congrats on making it this far, you deserve a hand! Look around, and you'll see lots of hands, imprinted by children of families that have supported Boyle Park. Can you find three hands together?",
+		"q":"Starting on the square with three hands, move nine squares to the right. What is the sum of the numbers on that square?",
+		"a":"2048,",
+		"h":"ask an adult to use their calculator",
+		"r":"Right!"
+	},
+	{
+		"f":"*Rock* solid job everyone. Debbie Joseph and her family would be proud. Maybe someday you'll have a plaque too.",
+		"q":"The dedication on this plaque is 8 lines long.\nIf you were to number each line of this dedication, 1 through 8, and each letter of each line, starting with 1 on each line, what secret word is revealed by the following cypher?\n7-3  1-16  5-10  3-3  7-4  5-15  3-20  5-18",
+		"a":"baseball",
+		"h":"The 7th line starts with the word 'Debbie', the 3rd letter of which is 'b'. So the answer starts with 'b'.",
+		"r":"Right!"
+	},
+	{
+		"f":"This game has a lot of ups and downs. Speaking of going up and down, find the only set of stairs in Boyle Park not near a baseball diamond.",
+		"q":"Standing at the top of the stairs, you can see 5 tennis courts below. Including all 5 courts, how many rectangles are there on the dark green parts of the courts?",
+		"a":"40",
+		"h":"There are 8 per court",
+		"r":"Right!"
+	},
+	{
+		"f":"You're good at this, I'm getting MAD. MAD, MAD, MAD! I feel like creating the word MAD somewhere!",
+		"q":"There are other words you can make using this tool. Can you make a word that goes with BBQ?",
+		"a":"pit",
+		"h":"In the land of the sand, toddlers can spell words.\nBBQ's often need a hollow indentation or hole in the ground, otherwise known as a ___. ",
+		"r":"Right!"
+	},
+	{
+		"f":"BBQ time. Were in the park can you find at least three BBQs close together?",
+		"q":"Assuming there's not a party going on, grab a seat, you've got a puzzle to solve!\nHow many different words can be made by removing letters from the phrase \n\"Mill Valley\"\nwithout rearranging any of the letters? (you can ignore the space in between)\n<i>For example, \"Sand\" could be made into 'sad', 'and', 'an', but not 'as'</i>.",
+		"a":"13",
+		"h":"There are more than 10 words. Did you see 'lay', 'vale' and 'alley'?",
+		"r":"Right!\n(mill, mile, mall, male, may, me, my, ill, lay, vale, valley, all, alley)"
+	},
+	{
+		"f":"A sign made of wood provides the phone number to reserve the Boyle Park picnic area. Find this sign.",
+		"q":"Writing the letter F requires three strokes, the top line, middle line and vertical line. How many strokes did the person carving this sign have to make? ",
+		"a":"73",
+		"h":"It more than 70 strokes! The number 8 took three strokes.",
+		"r":"Right!"
+	},
+	{
+		"f":"Boyle Park is cut in half by a creek. Today, you can cross this creek by a wooden footbridge. A much older footbridge, though, has since been torn down. You can still find its cement supports on the side of the creek. Find these cement supports.",
+		"q":"The name of what animals is written in the cement?",
+		"a":"lions",
+		"h":"You may have to look on both sides of the creek.",
+		"r":"Right!"
+	},
+	{
+		"f":"Cross the wooden footbridge (not the one that was torn down!) to reach the west bank of the creek, if you're not already there. Find the sign with drawings of *both* fish and birds.",
+		"q":"How many circles do you see?",
+		"a":"15",
+		"h":"There are more than a dozen, but less than 20.",
+		"r":"Right!"
+	},
+	{
+		"f":"Speaking of fish, find another sign near the creek with a drawing of fish.",
+		"q":"What is the sum of the numbers found on this sign (adding each number individually)?",
+		"a":"47",
+		"h":"It is also the sum of 10+9+8+7+6+5+4-3+2-1.",
+		"r":"Right! Ok, math class is over."
+	},
+	{
+		"f":"Oh say can you see?",
+		"q":"The flag is dedicated to Arthur Fontes, who graduated from Tam High School in 1939. He went on to fight in World War II for the Navy in the Pacific, before he was transferred to a military base in Chicago. But in his younger days, was a great athlete and baseball player. In his final few games playing for the North Bay League, Arthur had 13 hits in a row, including 5 home runs. What is the *minimum* number of bases that Arthur touched?",
+		"A":"29",
+		"h":"He hit 5 home runs touching 4 bases each, and another 8 hits worth at least 1 base each. And was there another base mentioned in the question?",
+		"r":"Right!"
+	},
 ];
 
 var finalQuestion = {

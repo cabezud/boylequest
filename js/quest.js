7bf88d5a726fcb8676ee2b7e3c92dcec4412d71a
cabezud <cabezud@hotmail.com>
2019-03-17 12:40:05 -0700

    fix line breaks

--- js/quest.js
+++ js/quest.js
@@ -4,14 +4,14 @@ var quest = [
 		"q":"Mill Valley Little League provides a sign with some advice for people watching a game. How many different *roles* are mentioned in their advice?",
 		"a":"6",
 		"h":"Don't forget the 'players' and 'professionals'",
-		"r":"Right!\n6 (players, professionals, managers, coaches, umpires, volunteers)"
+		"r":"Right!<br>6 (players, professionals, managers, coaches, umpires, volunteers)"
 	},
 	{
 		"f":"If both baseball fields are taken, players sometimes use a practice chain-link backstop to warm up. Find this practice area, and the bench near it dedicated to Marga Wells McLeod.",
 		"q":"When very large redwood trees are cut down, a group of smaller trees may grow in its place, sometimes called a 'fairy ring'. There is a very large such ring behind the bench where you are standing. How many (live) trees have grown in this grove?",
 		"a":"26",
 		"h":"Count anything any redwood truck that is growing out of the ground, even if it's skinny. The answer is an even number, and more than 20. ",
-		"r":"Right!\nI can't believe you counted all those trees!?"
+		"r":"Right!<br>I can't believe you counted all those trees!?"
 	},
 	{
 		"f":"Now, don't move! Well, you can turn around if you want. From where you are standing, can you see a yellow diamond shape? Go there!",
@@ -24,7 +24,7 @@ var quest = [
 		"f":"Baseball players have a lot a equipement when they play. Where do they store their stuff when they are playing?",
 		"q":"How many hooks can you count?",
 		"a":"44",
-		"h":"Don't forget the other dugout!",
+		"h":"It's more than 40. Don't forget the other dugout!",
 		"r":"Right!"
 	},
 	{
@@ -43,7 +43,7 @@ var quest = [
 	},
 	{
 		"f":"After all this running around - I have to go to the bathroom - I choose the one near the baseball diamonds. While I am there, I want to read about the restoration of Boyle Park in 1994!",
-		"q":"On the plaque, imagine that each line of text has a number assigned to it - 1-31.  And if you gave a number to each letter starting from the left, follow this cipher - what does it spell?\n4-24  11-7  2-15  9-56  13-5  5-3  1-3",
+		"q":"On the plaque, imagine that each line of text has a number assigned to it - 1-31.  And if you gave a number to each letter starting from the left, follow this cipher - what does it spell?<br>4-24  11-7  2-15  9-56  13-5  5-3  1-3",
 		"a":"varsity",
 		"h":"The 4th line starts with the word 'Terry', the 24th letter of which is ‘v’. So the answer starts with ‘v’",
 		"r":"Right!"
@@ -60,25 +60,25 @@ var quest = [
 		"q":"Note the hollow burned out trunk of the large bay tree. In the Fall of 2000, vandals set this tree on fire, but tree experts said the tree has been through several fires and is in no immediate danger. Take some free points and hear more about this great tree. Type 'fire' in the answer box. ",
 		"a":"fire",
 		"h":"The answer is 'fire'",
-		"r":"Right!\nMany believe this tree was one of three prominent trees that stood here since the 1800s. One of the three, an oak, died and was removed several years ago. The other, a very large and ancient looking oak, can be seen across the creek from this tree."
+		"r":"Right!<br>Many believe this tree was one of three prominent trees that stood here since the 1800s. One of the three, an oak, died and was removed several years ago. The other, a very large and ancient looking oak, can be seen across the creek from this tree."
 	},
 	{
 		"f":"Find the sand meant for kids.",
 		"q":"Two pyramids stand in the sky with moss growing on only one side. Which side?",
 		"a":"north",
 		"h":"north, south, east, or west?",
-		"r":"Right!\nFun fact, moss often grows on the north side of trees because there is more shade (in the northern hemisphere)"
+		"r":"Right!<br>Fun fact, moss often grows on the north side of trees because there is more shade (in the northern hemisphere)"
 	},
 	{
 		"f":"Congrats on making it this far, you deserve a hand! Look around, and you'll see lots of hands, imprinted by children of families that have supported Boyle Park. Can you find three hands together?",
 		"q":"Starting on the square with three hands, move nine squares to the right. What is the sum of the numbers on that square?",
-		"a":"2048,",
+		"a":"2048",
 		"h":"ask an adult to use their calculator",
 		"r":"Right!"
 	},
 	{
 		"f":"*Rock* solid job everyone. Debbie Joseph and her family would be proud. Maybe someday you'll have a plaque too.",
-		"q":"The dedication on this plaque is 8 lines long.\nIf you were to number each line of this dedication, 1 through 8, and each letter of each line, starting with 1 on each line, what secret word is revealed by the following cypher?\n7-3  1-16  5-10  3-3  7-4  5-15  3-20  5-18",
+		"q":"The dedication on this plaque is 8 lines long.<br>If you were to number each line of this dedication, 1 through 8, and each letter of each line, starting with 1 on each line, what secret word is revealed by the following cypher?<br>7-3   1-16   5-10   3-3   7-4   5-15   3-20   5-18",
 		"a":"baseball",
 		"h":"The 7th line starts with the word 'Debbie', the 3rd letter of which is 'b'. So the answer starts with 'b'.",
 		"r":"Right!"
@@ -94,15 +94,15 @@ var quest = [
 		"f":"You're good at this, I'm getting MAD. MAD, MAD, MAD! I feel like creating the word MAD somewhere!",
 		"q":"There are other words you can make using this tool. Can you make a word that goes with BBQ?",
 		"a":"pit",
-		"h":"In the land of the sand, toddlers can spell words.\nBBQ's often need a hollow indentation or hole in the ground, otherwise known as a ___. ",
+		"h":"In the land of the sand, toddlers can spell words.<br>BBQ's often need a hollow indentation or hole in the ground, otherwise known as a ___. ",
 		"r":"Right!"
 	},
 	{
 		"f":"BBQ time. Were in the park can you find at least three BBQs close together?",
-		"q":"Assuming there's not a party going on, grab a seat, you've got a puzzle to solve!\nHow many different words can be made by removing letters from the phrase \n\"Mill Valley\"\nwithout rearranging any of the letters? (you can ignore the space in between)\n<i>For example, \"Sand\" could be made into 'sad', 'and', 'an', but not 'as'</i>.",
+		"q":"Assuming there's not a party going on, grab a seat, you've got a puzzle to solve!<br>How many different words can be made by removing letters from the phrase <br>\"Mill Valley\"<br>without rearranging any of the letters? (you can ignore the space in between)<br><i>For example, \"Sand\" could be made into 'sad', 'and', 'an', but not 'as'</i>.",
 		"a":"13",
 		"h":"There are more than 10 words. Did you see 'lay', 'vale' and 'alley'?",
-		"r":"Right!\n(mill, mile, mall, male, may, me, my, ill, lay, vale, valley, all, alley)"
+		"r":"Right!<br>(mill, mile, mall, male, may, me, my, ill, lay, vale, valley, all, alley)"
 	},
 	{
 		"f":"A sign made of wood provides the phone number to reserve the Boyle Park picnic area. Find this sign.",
@@ -119,7 +119,7 @@ var quest = [
 		"r":"Right!"
 	},
 	{
-		"f":"Cross the wooden footbridge (not the one that was torn down!) to reach the west bank of the creek, if you're not already there. Find the sign with drawings of *both* fish and birds.",
+		"f":"Make your way to the west bank of the creek, if you're not already there. Find the sign with drawings of *both* fish and birds.",
 		"q":"How many circles do you see?",
 		"a":"15",
 		"h":"There are more than a dozen, but less than 20.",
@@ -129,20 +129,20 @@ var quest = [
 		"f":"Speaking of fish, find another sign near the creek with a drawing of fish.",
 		"q":"What is the sum of the numbers found on this sign (adding each number individually)?",
 		"a":"47",
-		"h":"It is also the sum of 10+9+8+7+6+5+4-3+2-1.",
+		"h":"It is also the sum of 10+9+8+7+6+5+2.",
 		"r":"Right! Ok, math class is over."
 	},
 	{
 		"f":"Oh say can you see?",
-		"q":"The flag is dedicated to Arthur Fontes, who graduated from Tam High School in 1939. He went on to fight in World War II for the Navy in the Pacific, before he was transferred to a military base in Chicago. But in his younger days, was a great athlete and baseball player. In his final few games playing for the North Bay League, Arthur had 13 hits in a row, including 5 home runs. What is the *minimum* number of bases that Arthur touched?",
-		"A":"29",
+		"q":"The flag is dedicated to Arthur Fontes, who graduated from Tam High School in 1939. He went on to fight in World War II for the Navy in the Pacific, before he was transferred to a military base in Chicago. But in his younger days, was a great athlete and baseball player. In his final few games playing for the North Bay League, Arthur had 8 hits and 5 home runs. What is the *minimum* number of bases that Arthur touched?",
+		"a":"29",
 		"h":"He hit 5 home runs touching 4 bases each, and another 8 hits worth at least 1 base each. And was there another base mentioned in the question?",
 		"r":"Right!"
 	},
 ];
 
 var finalQuestion = {
-	"f":"FINAL QUESTION!!! No pressure, but this one is worth 50 points!\nReturn to the location of the three BBQs together.",
+	"f":"FINAL QUESTION!!! No pressure, but this one is worth 50 points!<br>Return to the location of the three BBQs together.",
 	"q":"In what year was Robert born?",
 	"a":"2009",
 	"h":"What has been at the bottom of this screen all along?",
